9820088064f05a80f06958f1359109966312174a
Tinh La <tla005@ucr.edu>
2019-02-10 12:09:02 -0800

    Created manager.cpp

--- src/manager.cpp
+++ src/manager.cpp
@@ -1,1 +1,18 @@
+#include "../header/manager.h"
 
+void Manager::run() 
+{
+	char userInput[1024];
+
+	while(true)
+	{
+		memset(userInput, 0, sizeof(userInput));
+		cout << "$ ";
+		cin.getline(userInput, 1024);
+		userInput[cin.gcount()] = '\0';
+		cout << endl;
+	}
+	Token userLine;
+	queue<Token> delimited_token_queue, main_token_queue;
+	Parse limit(userInput, ';', false, false);
+}
