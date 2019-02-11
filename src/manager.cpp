2280663483dd004356789c1e50f7852a9e943ede
ppattab24 <46436892+ppattab24@users.noreply.github.com>
2019-02-10 17:03:35 -0800

    finished execute functions

--- src/manager.cpp
+++ src/manager.cpp
@@ -40,3 +40,51 @@ void Manager::run()
 		cout << endl;
 	}
 }
+
+void Manager::execute(string command_string) {
+	
+	char* cString = str_to_char(command_string);
+	char* command[64];
+	memset(command, 0, sizeof(command));
+	
+	parse(command_string, command);
+	
+	execute(command);
+	
+	delete[] cString;
+	
+	
+}
+
+void Manager::execute(char** command) {
+	
+	pid_t process_id;	
+	int status;
+	
+	if (equals(*command, "exit", false)) {
+		exit(0);
+	}
+	
+	if ((process_id = fork()) < 0) {
+		exit(1)
+	}
+	else if (process_id == 0) {
+		if (execvp(*command, command) < 0) {
+			cerr<< "Command did not execute:failed" << endl;
+			worked = false;
+		}
+		else {
+			worked = true;	
+		}
+	}
+	else {
+		worked = true;
+		while (wait(&status) != process_id);
+		
+		if (WEXITSTATUS(status)) {
+			worked = false;
+		}
+	}
+	
+	
+}
