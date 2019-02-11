7ae123694ac15482e233cbc64b2ca473b19e7556
TinhLa7 <43663011+TinhLa7@users.noreply.github.com>
2019-02-11 15:54:15 -0800

    Update parse.cpp

--- src/parse.cpp
+++ src/parse.cpp
@@ -1,121 +1,154 @@
 #include "../header/parse.h"
 
-Parse::Parse() 
-{
-	_initialize(NULL, ' ', true, false);
+Parse::Parse() {
+
+    _init(NULL, ' ', true, false);
 }
 
-Parse::Parse(char* cstring, char delimiter, bool quotesSeparate, bool Tests)
-{
-	_initialize(cstring, delimiter, quotesSeparate, Tests);
+Parse::Parse(char *cstring, char parser, bool quotesSeparately, bool Tests) {
+
+    _init(cstring, parser, quotesSeparately, Tests);
 }
 
-Parse::Parse(string str, char delimiter, bool quotesSeparate, bool Tests)
-{
-	str = padDelim(str, '(');
-	str = padDelim(str, ')');
+Parse::Parse(string str, char parser, bool quotesSeparately, bool Tests) {
 
-	char* cstring = str_to_char(str);
-	_initialize(cstring, delimiter, quotesSeparate, Tests);
-	delete [] cstring;
+    str = padParse(str, '(');
+    str = padParse(str, ')');
+
+    char* c = str_to_char(str);
+    _init(c, parser, quotesSeparately, Tests);
+    delete [] c;
 }
 
 bool Parse::done() const { return q.empty(); }
 
 size_t Parse::size() const { return q.size(); }
 
-void Parse::compressContents()
-{
-	queue<Token> replacementQueue;
+void Parse::compressTokens() {
 
-	while(!q.empty())
-	{
-		vector<Token> combinationVector;
-		Token firstToken = q.front();
-	        combinationVector.push_back(firstToken);
-		q.pop();
+    queue<Token> replacementQueue;
 
-		if(!_isGoodCommand(firstToken)) 
-		{
-			while(!q.empty() && (!_isGoodCommand(q.front()) || firstToken.toString().at(0) == '\"')) 
-			{
-				Token addToken = q.front();
-				combinationVector.push_back(addToken);
-				q.pop();
 
-				if(firstToken.toString().at(0) == '\"' && addToken.toString().at(addToken.toString().size() - 1) == '\"') break;
-			}
-		}
+    while(!q.empty()) {
 
-		Token combinedToken(combinationVector);
+        //Prepare to append to current Token
+        vector<Token> aggregateVector;
+        Token initialToken = q.front();
+        aggregateVector.push_back(initialToken);
+        q.pop();
 
-		if(combinedToken.isTest())
-			combinedToken._pruneTest();
-		replacementQueue.push(combinedToken);	
-	}
-	addItems(q, replacementQueue);
-	clearAll(replacementQueue);
+
+
+        //Only consider appending if it's not blacklisted
+        if (!_isBlacklisted(initialToken)) {
+
+
+            while (!q.empty()
+
+                   //              //Iterate through q for as long as the type matches initialToken
+                   //              //Or, iterate through q for as long as the quotations extends
+                   //              && (q.front().getStatus() == initialToken.getStatus() || initialToken.toString().at(0) == '\"')) {
+
+                   //Iterate thorugh q for as long as we don't reach something that we should delimit
+                   //Or, iterate through q for as long as the quotations extends
+                   && (!_isBlacklisted(q.front()) || initialToken.toString().at(0) == '\"')) {
+
+                Token appendMe = q.front();
+                aggregateVector.push_back(appendMe);
+                q.pop();
+
+                //Exit the loop if we were in a quotations, and there's a \" at the end of this token to end the quotations
+                if (initialToken.toString().at(0) == '\"'
+                    && appendMe.toString().at(appendMe.toString().size() - 1) == '\"')
+                    break;
+            }
+
+        }
+
+        //Make the combine token based on what you collected, and push it into replacementQueue
+        Token aggregateToken(aggregateVector);
+
+        if(aggregateToken.isTest())
+            aggregateToken._pruneTest();
+
+        replacementQueue.push(aggregateToken);
+    }
+
+
+
+    //Replace queue
+    addItems(q, replacementQueue);
+    clearAll(replacementQueue);
+//    q.swap(replacementQueue);
+//    assert(replacementQueue.empty());
 }
 
-Parse& operator >>(Parse& delim, Token& t)
-{
-	assert(!delim.q.empty());
-	t = delim.q.front();
-	delim.q.pop();
-	return delim;
+Parse& operator >>(Parse& parser, Token& t) {
+
+    assert(!parser.q.empty());
+
+    t = parser.q.front();
+    parser.q.pop();
+
+    return parser;
 }
 
-ostream& operator <<(ostream& outs, const Parse& d) 
-{
-	queue<Token> qCopy(d.q);
-	outs << "{ ";
-	while(!qCopy.empty())
-	{
-		outs << "(" << qCopy.front() << ") ";
-		qCopy.pop();
-	}
+ostream& operator <<(ostream& outs, const Parse& d) {
 
-	outs << "}";
-	return outs;
+    queue<Token> qCopy(d.q);
+
+    outs << "{ ";
+
+    while(!qCopy.empty()) {
+
+        outs << "(" << qCopy.front() << ") ";
+        qCopy.pop();
+    }
+
+    outs << "}";
+
+    return outs;
 }
 
-void Parse::_initialize(char *cstring, char delim, bool separateQuotes, bool Tests) {
+void Parse::_init(char *cstring, char parser, bool quotesSeparately, bool Tests) {
 
     if (cstring == NULL)
         return;
 
-    char* temp = cstring;
-    bool careAboutQuotes = separateQuotes&& _Quotes(cstring);
+    char* walker = cstring;
+    bool weCareAboutQuotes = quotesSeparately && _Quotes(cstring);
 
-    while(*temp != '\0') {
+    //Walk through the cstring
+    while(*walker != '\0') {
 
-        string currentString = "";
+        string currentStr = "";
         bool inQuoteFlag = false, commentFound = false;
 
-        while (*temp != '\0' && ( (*temp != delim) || (inQuoteFlag && careAboutQuotes)) ) {
+        //Don't delimit if we're in a quotations!
+        while (*walker != '\0' && ( (*walker != parser) || (inQuoteFlag && weCareAboutQuotes)) ) {
 
-            char currChar = *temp;
+            char currentChar = *walker;
 
-            if (currChar == '\"')
+            if (currentChar == '\"')
                 inQuoteFlag = !inQuoteFlag; //toggle whether we're in or not
 
-            if (currChar == '#' && !inQuoteFlag) {
+            if (currentChar == '#' && !inQuoteFlag) {
                 commentFound = true;
                 break;
             }
 
-            currentString += currChar;
+            currentStr += currentChar;
 
-            temp++;
+            walker++;
 
         }
 
-        if (!currentString.empty())
+        if (!currentStr.empty())
         {
-            if ( (!q.empty()) && (q.back().isTest()) && (Tests) && (currentString != "&&" && currentString != "||") )
-                q.push(Token(currentString, q.back().getStatus()));
+            if ( (!q.empty()) && (q.back().isTest()) && (Tests) && (currentStr != "&&" && currentStr != "||") )
+                q.push(Token(currentStr, q.back().getStatus()));
             else
-                q.push(Token(currentString, Tests));
+                q.push(Token(currentStr, Tests));
         }
 
 
@@ -123,73 +156,80 @@ void Parse::_initialize(char *cstring, char delim, bool separateQuotes, bool Tes
         if (commentFound)
             return;
 
-        if (*temp != '\0')
-            temp++;
+        if (*walker != '\0')
+            walker++;
     }
 
 }
 
-bool Parse::_Quotes(char* cstring) {
+bool Parse::_Quotes(char* c) {
 
-    string str(cstring);
+    string str(c);
     return _Quotes(str);
 }
 
 bool Parse::_Quotes(string str) {
 
-    size_t totalInput = 0;
+    size_t totalQuotes = 0;
 
     for (size_t i = 0; i < str.size(); i++) {
 
         if (str[i] == '\"')
-            totalInput++;
+            totalQuotes++;
     }
 
-    return totalInput % 2 == 0;
+    return totalQuotes % 2 == 0;
 }
 
-void Parse::reinitializeStates()
+void Parse::checkFlagsAndReinitStatus()
 {
-    queue<Token> replacement;
+    queue<Token> replacementQueue;
 
     if(q.front().toString().substr(0,1) == "[")
         q.front()._pruneTest();
 
     while(!q.empty())
     {
-        Token currToken = q.front();
+        Token currentToken = q.front();
 
-        if(currToken.isTest())
+        if(currentToken.isTest())
         {
-            if(currToken.toString().substr(0, 2) == "-d")
+            if(currentToken.toString().substr(0, 2) == "-d")
             {
-                currToken.setStatus(Token::test1);
+                currentToken.setStatus(Token::test1);
 
-                string currToken_string = currToken.toString();
-                currToken_string.erase(0, 3);
-                currToken.setString(currToken_string);
+                string currentTok_str = currentToken.toString();
+                currentTok_str.erase(0, 3);
+                currentToken.setString(currentTok_str);
             }
-            else if(currToken.toString().substr(0, 2) == "-f")
+            else if(currentToken.toString().substr(0, 2) == "-f")
             {
-                currToken.setStatus(Token::test3);
+                currentToken.setStatus(Token::test3);
 
-                string currToken_string = currToken.toString();
-                currToken_string.erase(0, 3);
-                currToken.setString(currToken_string);
+                string currentTok_str = currentToken.toString();
+                currentTok_str.erase(0, 3);
+                currentToken.setString(currentTok_str);
             }
         }
 
-        replacement.push(currToken);
+        replacementQueue.push(currentToken);
         q.pop();
     }
+
+
+    //Replace queue
+//    q.swap(replacementQueue);
+//    assert(replacementQueue.empty());
+    addItems(q, replacementQueue);
+    clearAll(replacementQueue);
 }
 
-bool Parse::_isGoodCommand(Token t) {
+bool Parse::_isBlacklisted(Token t) {
 
-    size_t currentTokenStatus = t.getStatus();
+    size_t thisStatus = t.getStatus();
 
-    return currentTokenStatus == Token::lParren 
-            || currentTokenStatus == Token::rParren
-            || currentTokenStatus == Token::connector
-            || currentTokenStatus == Token::error;
+    return thisStatus == Token::lParren
+            || thisStatus == Token::rParren
+            || thisStatus == Token::connector
+            || thisStatus == Token::error;
 }
