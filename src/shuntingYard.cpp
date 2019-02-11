cf9644307f1e5935f8f0aa6ea0d48fc91f90c3d1
TinhLa7 <43663011+TinhLa7@users.noreply.github.com>
2019-02-11 15:55:03 -0800

    Update shuntingYard.cpp

--- src/shuntingYard.cpp
+++ src/shuntingYard.cpp
@@ -1,54 +1,64 @@
 #include "../header/shuntingYard.h"
 
-ShuntingYard::ShuntingYard(queue<Token> commandQ)
+shuntingYard::shuntingYard(queue<Token> command)
 {
-    delivery = commandQ;
+    delivery = command;
     run();
 }
 
-void ShuntingYard::run()
+void shuntingYard::run()
 {
-    while(!delivery.empty())    
+    while(!delivery.empty())    //while there are still tokens to be read
     {
         Token currentToken = delivery.front();
         delivery.pop();
 
+        //if token is a command, push it into the command stack
         if( currentToken.getStatus() == Token::middle || currentToken.getStatus() == Token::quotations
                 || currentToken.isTest() )
-            command_token_queue.push(currentToken);
+            command_queue.push(currentToken);
         if(currentToken.getStatus() == Token::connector)
         {
-            while(!opStack.empty() && opStack.top().getStatus() != Token::lParren)
-            {
-                command_token_queue.push(opStack.top());
-                opStack.pop();
-            }
+            //else, if the precedence of the incoming token is less than the token at the
+            //top of the operator_stack, pop tokens from the stack and push it into the command_queue
 
-            opStack.push(currentToken);
+            //NOTE: there are 4 seperate cases of either the && or || connectors popping up
+            //(&& ||, || ||, || &&, and && &&). However, since && takes precedence over ||, there is
+            //only one case where that happens (if an && connector coming in from delivery is being compared
+            //with an || connector at the top of the stack).
+            while(!operator_stack.empty() && operator_stack.top().getStatus() != Token::lParren)
+            {
+                command_queue.push(operator_stack.top());
+                operator_stack.pop();
+            }
+            //if the precedence of the incoming token is less than the token at the top of the operator_stack,
+            //simply push that token into the stack
+            operator_stack.push(currentToken);
         }
 
         if(currentToken.getStatus() == Token::lParren)
-            opStack.push(currentToken);
+            operator_stack.push(currentToken);
 
         if(currentToken.getStatus() == Token::rParren)
         {
-            while(opStack.top().getStatus() != Token::lParren)
+            while(operator_stack.top().getStatus() != Token::lParren)
             {
-                command_token_queue.push(opStack.top());
-                opStack.pop();
+                command_queue.push(operator_stack.top());
+                operator_stack.pop();
             }
 
-            assert(!opStack.empty());
-            opStack.pop();  
+            assert(!operator_stack.empty());
+            operator_stack.pop();  //pops left bracket
         }
     }
 
-    while(!opStack.empty())
+    //once all tokens have been read, push the remaining connectors in the stack to the queue
+    while(!operator_stack.empty())
     {
-        command_token_queue.push(opStack.top());
-        opStack.pop();
+        command_queue.push(operator_stack.top());
+        operator_stack.pop();
     }
 
 }
 
-queue<Token> ShuntingYard::getReversie() { return command_token_queue; }
+queue<Token> shuntingYard::getReversePolish() { return command_queue; }
