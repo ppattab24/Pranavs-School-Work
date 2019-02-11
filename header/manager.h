87c324856797d5b69eba7aa174f166156237b6df
TinhLa7 <43663011+TinhLa7@users.noreply.github.com>
2019-02-11 15:47:08 -0800

    Update manager.h

--- header/manager.h
+++ header/manager.h
@@ -1,42 +1,47 @@
 #ifndef MANAGER_H
 #define MANAGER_H
 
-#include <string>
-#include <cstring>
+
+#include <iostream>
 #include <stack>
 #include <queue>
 #include <unistd.h>
+#include <cstring>
 #include <cstdlib>
+#include <string>
 #include <climits>
 #include <cassert>
 #include <vector>
 #include <fstream>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <sys/wait.h>
-#include <iostream>
+#include <sys/wait.h> //uncomment for hammer
+
 #include "token.h"
 #include "parse.h"
-#include "genuse.h"
+#include "util.h"
 #include "shuntingYard.h"
 
 using namespace std;
 
-class Manager {
-     public:
-	void run();
-     private:
-	void execute(char **command);
-	void execute(string command);
-	void parse(char*, char **);
-	bool shouldExecute(vector<Token> commands);
-	void evaluate(vector<Token> binExpression);
-	void evalPostFix(queue<Token>& postfix_queue);
-	queue<Token> combineCommands(queue<Token>& old_queue);
-	bool isThisADirectory(string pathname);
-	bool isThisAFile(string pathname);
+class Manager
+{
+public:
+    void run();
+private:
+    void execute(char **command);
+    void execute(string command);
+    void parse(char *, char **);
+    bool shouldExecute(vector<Token> expr);
+    void evaluate(vector<Token> binExpression);
+    void evalPostFix(queue<Token>& token_postfix_queue);
+    queue<Token> combineCommands(queue<Token>& old_token_queue);
+    bool isThisADirectory(string pathname);
+    bool isThisAFile(string pathname);
 
-	bool successful;
+    bool wasSuccess;
 };
 
-#endif
+
+#endif //MANAGER_H
+
